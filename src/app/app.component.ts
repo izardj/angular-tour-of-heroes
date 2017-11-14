abf4ebac0ef6b62bf921390ae8376f2e32c861b6
Jérôme Izard <jerome.izard@econocom.com>
2017-11-14 16:05:21 +0100

    Part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,7 @@ const HEROES: Hero[] = [
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <app-heroes></app-heroes>
     <ul class="heroes">
       <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero" >
         <span class="badge">{{hero.id}}</span> {{hero.name}}
